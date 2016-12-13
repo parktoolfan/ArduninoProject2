196ac6318ec36924ad5441f0d25a0a00580201a3
Kona <theostangebye@gmail.com>
2016-12-12 20:40:20 -0500

    Added ICP protocol files
    Going to start ICP now

---
+++ JosiahICP/JosiahICP.ino
@@ -0,0 +1,242 @@
+
+int speakerPin = 13;
+
+//////////////////////////////////////////////////////////////////////////
+const int dotBeat = 60; // For our purposes, the dot beat is a single unit of time (in morse code, a dot is one unit, while a dash is three unts).
+const int dashBeat = 3 * dotBeat;
+
+int length = 1; // the number of notes
+char notes[] = "C"; // a space represents a rest
+int beats[] = {1};
+int tempo = 300;
+
+void playTone(int tone, int duration) {
+  for (long i = 0; i < duration * 1000L; i += tone * 2) {
+    digitalWrite(speakerPin, HIGH);
+    delayMicroseconds(tone);
+    digitalWrite(speakerPin, LOW);
+    delayMicroseconds(tone);
+  }
+}
+
+void playNote(char note, int duration) {
+  char names[] = { 'C' };
+  int tones[] = { 956 };
+  
+  // play the tone corresponding to the note name
+
+  playTone(tones[0], duration);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+const int buttonPin1 = 2; 
+const int buttonPin2 = 3;
+const int buttonPin3 = 4;
+const int ledPin1 = 8;       
+const int ledPin2 = 9;
+const int ledPin3 = 10;
+
+int buttonPushCounter1 = 0;   
+int buttonState1 = 0;        
+int lastButtonState1 = 0;   
+
+int buttonPushCounter2 = 0; 
+int buttonState2 = 0;
+int lastButtonState2 = 0;
+
+int buttonPushCounter3 = 0; 
+int buttonState3 = 0;
+int lastButtonState3 = 0;
+
+// Array to hold the chars 
+char singleMorse[] = "     ";
+char* morseCode[] = {".-   ", "-... ", "-.-. ", "-..  ", ". ", "..-. ", "--.  ", ".... ", "..   ", // A-I
+".--- ", "-.-  ", ".-.. ", "--   ", "-.   ", "---  ", ".--. ", "--.- ", ".-.  ", // J-R 
+"...  ", "-    ", "..-  ", "...- ", ".--  ", "-..- ", "-.-- ", "--.. ", // S-Z
+"-----", ".----", "..---", "...--", "....-", ".....", // 0-5
+"-....", "--...", "---..", "----.", // 6-9
+"     "
+};
+
+// This array contains the letters and numbers that the user input will be translated to.
+char alphaNum[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+'0', '1', '2', '3', '4', '5',
+'6', '7', '8', '9', '_'};
+
+// This array contains the final user text:
+char userText[] = "                              ";
+// Variable to keep track of where to add characters to userText.
+int textIndex = 0;
+char charIndex = 'a';
+
+void setup() {
+ pinMode(speakerPin, OUTPUT);
+ 
+ pinMode(buttonPin1, INPUT);
+ pinMode(ledPin1, OUTPUT);
+ Serial.begin(9600);
+ 
+ pinMode(buttonPin2, INPUT);
+ pinMode(ledPin2, OUTPUT);
+ Serial.begin(9600);
+
+ pinMode(buttonPin3, INPUT);
+ pinMode(ledPin3, OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+ buttonState1 = digitalRead(buttonPin1);
+
+ if (buttonState1 != lastButtonState1) {
+   if (buttonState1 == LOW) {
+     // First, turn on the LED briefly to signify that the button was pressed.
+     digitalWrite(ledPin1, HIGH);   // turn the LED on (HIGH is the voltage level)
+     delay(dotBeat);
+     digitalWrite(ledPin1, LOW);
+     delay(dotBeat);
+     // Also, briefly play a note to correspond with the appropriate character:
+     playNote(notes[0], dotBeat);
+    
+ 
+     buttonPushCounter1++;
+     Serial.println("on");
+     Serial.print("number of 1st button pushes:  ");
+     Serial.println(buttonPushCounter1);
+     // We need to "translate" a button bush into the appropiate character: - or .
+     singleMorse[(buttonPushCounter1 + buttonPushCounter2) - 1] = '.';
+
+     Serial.print("Current overall button count: ");
+     Serial.println(buttonPushCounter1 + buttonPushCounter2);
+     if ((buttonPushCounter1 + buttonPushCounter2) == 5) { // We do this because the max number of morse characters for translation is 5 (for our purposes).
+        fullArray12(); // Call the function to translate the array
+     }
+     
+   } 
+   else {
+     Serial.println("off");
+   }
+   delay(50);
+ }
+ lastButtonState1 = buttonState1;
+
+
+ 
+ buttonState2 = digitalRead(buttonPin2);
+
+ if (buttonState2 != lastButtonState2) {
+   if (buttonState2 == LOW) {
+    // First, turn on the LED briefly to signify that the button was pressed.
+     digitalWrite(ledPin1, HIGH);   // turn the LED on (HIGH is the voltage level)
+     delay(dashBeat);
+     digitalWrite(ledPin1, LOW);
+     delay(dotBeat);
+
+     // Also, briefly play a note to correspond with the appropriate character:
+     playNote(notes[0], dashBeat); 
+     
+     buttonPushCounter2++;
+     Serial.println("on");
+     Serial.print("number of 2nd button pushes:  ");
+     Serial.println(buttonPushCounter2);
+    // We need to "translate" a button bush into the appropiate character: - or .
+     singleMorse[(buttonPushCounter1 + buttonPushCounter2) - 1] = '-';
+
+     Serial.print("Current overall button count: ");
+     Serial.println(buttonPushCounter1 + buttonPushCounter2);
+     if ((buttonPushCounter1 + buttonPushCounter2) == 5) {
+       fullArray12();
+     }
+   } 
+   else {
+     Serial.println("off");
+   }
+   delay(50);
+ }
+ lastButtonState2 = buttonState2;
+
+
+ buttonState3 = digitalRead(buttonPin3);
+ 
+ if (buttonState3 != lastButtonState3) {
+   if (buttonState3 == LOW) {
+     buttonPushCounter3++;
+     Serial.println("on");
+     Serial.print("number of 3rd button pushes:  ");
+     Serial.println(buttonPushCounter3);
+     fullArray12();
+   }
+   else {
+     Serial.println("off");
+   }
+   
+   delay(50);
+ }
+ lastButtonState3 = buttonState3;
+}
+
+void storeChar(char c) { // The function takes in an argument, a character, and stores it in the final user array.
+  userText[textIndex] = c;
+  textIndex++; // Move to next index for next char storage.
+}
+
+void fullArray12() {
+  // First, signal the blue light; this indicates that a letter is to be translated.
+
+  for(int fadeValue = 0 ; fadeValue <= 255; fadeValue +=51) { 
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin3, fadeValue);         
+    // wait for 30 milliseconds to see the dimming effect    
+    delay(30);                            
+    } 
+    // fade out from max to min in increments of 5 points:
+  for(int fadeValue = 255 ; fadeValue >= 0; fadeValue -=51) { 
+    // sets the value (range from 0 to 255):
+    analogWrite(ledPin3, fadeValue);         
+    // wait for 30 milliseconds to see the dimming effect    
+    delay(30);                            
+  } 
+  Serial.print("Current morse code entry: ");
+  for (int i = 0; i < 5; i++) {
+    Serial.print(singleMorse[i]);
+  }
+  Serial.println("");
+  // IMPORTANT: Translate: We do this by comparing the user input (singleMorse) a "library" of morse code Arrays.
+  for(int i = 0; i < 37; i++) {
+    if (strcmp(singleMorse, morseCode[i]) == 0) {
+      Serial.println("Match!");
+      Serial.println(alphaNum[i]); // Print the corresponding alphanumerical character.
+      charIndex = alphaNum[i]; // Store in variable.
+      storeChar(charIndex); // Call the function, and pass the variable to the functions parameters.
+      break;
+    }
+    else {
+    // No match found.
+      if (i == 36) {
+        digitalWrite(ledPin2, HIGH);   // turn the LED on (HIGH is the voltage level)
+        delay(30);
+        digitalWrite(ledPin2, LOW);
+        delay(30);
+      }
+    }
+  }
+  // Important: Clear the morse code user input.
+  for (int i = 0; i < 5; i++) {
+    singleMorse[i] = ' ';
+  }
+  Serial.print("User's current input (English): ");
+  for(int i = 0; i < 33; i++) {
+    Serial.print(userText[i]);
+  } 
+  buttonPushCounter1 = 0; // Reset buttonPushCounter1 to zero
+  buttonPushCounter2 = 0; // Reset buttonPushCounter2 to zero
+  Serial.println("");
+  Serial.println("(buttonPushCounters 1 and 2 have been reset to zero.)"); 
+  
+}
+
+
---
+++ TheoICP/TheoICP.ino
@@ -0,0 +1,250 @@
+#include <SPI.h>
+#include <Ethernet.h>
+byte mac[] = {
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+};
+
+//LCD Setup
+#include <SoftwareSerial.h>
+#include <ParallaxLCD.h>
+ParallaxLCD lcd(6, 2, 16);
+
+//Begin Amazon wireless setup
+char serverNameAmazon[] = "s3.amazonaws.com"; // zoomkat's test web page server  web.comporium.net
+
+//Begin Ifttt setup
+char serverName[] = "maker.ifttt.com";
+int totalCount = 0; //0
+// change to your server's port
+int serverPort = 80;
+// change to the page on that server
+char pageName[] = "/trigger/fromArduinoPayload/with/key/bLMBpfOKL5alWnYd1MMij3";
+char params[32];
+
+EthernetClient client;
+
+//Establish Program output pins
+const int buttonPin = 5;
+int buttonState = 0;
+
+//lastMessage will be the string that is the last text message recieved, we will compare this with newMessage to see if there has been a new text message.
+String lastMessage;
+String newMessage;
+
+void setup() {
+  Serial.begin(9600);
+  //Signal Power
+  pinMode(7, OUTPUT);
+  digitalWrite(7, HIGH);
+  pinMode(buttonPin, INPUT);
+
+  //start LCD
+  lcd.setup();
+  delay(500);
+  lcd.backLightOn();
+  lcd.empty();
+
+  //Start Ethernet
+  Serial.print(F("Starting ethernet..."));
+  if (!Ethernet.begin(mac)) Serial.println(F("failed"));
+  else Serial.println(Ethernet.localIP());
+  delay(2000);
+  Serial.println(F("Ready"));
+}
+
+void loop() {
+  //Begin lets try to put all this together
+  //Maintain ethernet:
+  Ethernet.maintain();
+  Serial.println("Looping");
+  //Blink LED
+  digitalWrite(4, HIGH);
+  delay(500);
+  digitalWrite(4, LOW);
+  delay(500);
+
+  //Check if there is a message to send and if so, send it.
+  buttonState = digitalRead(buttonPin);
+  Serial.print(buttonState);
+  if (buttonState == 1) {
+    // turn LED on:
+    //Blink LED 3 times
+    digitalWrite(4, HIGH);
+    delay(100);
+    digitalWrite(4, LOW);
+    delay(100);
+    digitalWrite(4, HIGH);
+    delay(100);
+    digitalWrite(4, LOW);
+    delay(100);
+    digitalWrite(4, HIGH);
+    delay(100);
+    digitalWrite(4, LOW);
+    delay(100);
+    doPost("Jacob, Sending is Done!");
+    buttonState = 0;
+  }
+
+  //Recieving code
+  sendGET(); //call send get. This will set value of newMessage.
+  if (!(newMessage.equals(lastMessage))) {
+    Serial.println("There is a New Message");
+    char output[32];
+    newMessage.toCharArray(output, 32);
+    toPrint(output);
+    //toPrint(output[]);
+    Serial.println();
+    lastMessage = newMessage;
+  } else {
+    Serial.println("No New Message.");
+    Serial.println();
+  }
+  //delay 10 seconds at end
+  delay(5000);
+}
+
+//Begin AMAZON S3 METHODS
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void sendGET() //client function to send/receive GET request data.
+{
+  if (client.connect(serverNameAmazon, 80)) {  //starts client connection, checks for connection
+    //Serial.println("connected");
+    Serial.println();
+    client.println("GET /arduinoproject/s3conversation.txt HTTP/1.1"); //download text
+    client.println("Host: s3.amazonaws.com");
+    client.println("Connection: close");  //close 1.1 persistent connection
+    client.println(); //end of get request
+  }
+  else {
+    Serial.println("connection failed"); //error message if no client connect
+    Serial.println();
+  }
+
+  while (client.connected() && !client.available()) delay(1); //waits for data to be downloaded
+
+  while (true) {
+    if (client.peek() == 'b') {
+      client.read();
+      if (client.peek() == 'g') {
+        client.read();
+        if (client.peek() == 'g') {
+          client.read();
+          if (client.peek() == 'n') {
+            client.read();
+            break;
+          }
+        }
+      }
+    } else {
+      client.read();
+    }
+  }
+  int a = 0;
+  char payloadArray[32];
+  //Now clear array.
+  for (int i = 0; i < sizeof(payloadArray); i = i + 1) {
+    payloadArray[i] = ' ';
+  }
+  while (client.connected() || client.available()) { //connected or data available
+    //    //client.readStringUntil(':');
+    char c = client.read(); //gets byte from ethernet buffer
+    //Serial.print(c); //prints raw feed for testing
+    payloadArray[a] = c;
+    a = a + 1;
+  }
+  Serial.print("Here is our array:     ");
+  for (int i = 0; i < sizeof(payloadArray); i = i + 1) {
+    Serial.print(payloadArray[i]);
+  }
+  Serial.println();
+
+  String str(payloadArray);
+  newMessage = payloadArray;
+
+  client.stop(); //stop client
+}
+
+//Begin IFTTT POST METHODS
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void doPost(String a) {
+
+  //Create Payload Message
+  String ident = "value1=3175636398&value2=";
+  String totalMessage = ident + a;
+
+  Serial.print("totalMessage: ");
+  Serial.println(totalMessage);
+
+  if (!postPage(serverName, serverPort, pageName, totalMessage.c_str())) Serial.print(F("Fail "));
+  else Serial.print(F("Pass "));
+    totalCount++;
+    Serial.println(totalCount,DEC);
+}
+
+byte postPage(char* domainBuffer, int thisPort, char* page, char* thisData)
+{
+  int inChar;
+  char outBuf[64];
+
+  Serial.print(F("connecting..."));
+
+  if (client.connect(domainBuffer, thisPort) == 1)
+  {
+    Serial.println(F("connected"));
+
+    // send the header
+    sprintf(outBuf, "POST %s HTTP/1.1", page);
+    client.println(outBuf);
+    sprintf(outBuf, "Host: %s", domainBuffer);
+    client.println(outBuf);
+    client.println(F("Connection: close\r\nContent-Type: application/x-www-form-urlencoded"));
+    sprintf(outBuf, "Content-Length: %u\r\n", strlen(thisData));
+    client.println(outBuf);
+
+    // send the body (variables)
+    client.print(thisData);
+    Serial.println("ThisData: ");
+    Serial.println(thisData);
+  }
+  else
+  {
+    Serial.println(F("failed"));
+    return 0;
+  }
+
+  int connectLoop = 0;
+
+  while (client.connected())
+  {
+    while (client.available())
+    {
+      inChar = client.read();
+      Serial.write(inChar);
+      connectLoop = 0;
+    }
+
+    delay(1);
+    connectLoop++;
+    if (connectLoop > 10000)
+    {
+      Serial.println();
+      Serial.println(F("Timeout"));
+      client.stop();
+    }
+  }
+
+  Serial.println();
+  Serial.println(F("disconnecting."));
+  client.stop();
+  return 1;
+}
+
+//Begin LCD Methods
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+void toPrint(char output[]) {
+  lcd.empty();
+  lcd.at(0, 0, output);
+  delay(5000);
+}
